ebc6808f9814c19c59ddc999c56f138312583bc9
Neskoff <neskoff@gmail.com>
2021-07-28 12:35:55 +0200

    Routing Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "../assets/styles/components/Header.scss";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
-      <span className="accent">Air</span>vironment
+      <Link path="/test">
+        <span className="accent">Air</span>vironment
+      </Link>
       <ul>
         <li>
-          <a href="#">All Data</a>
+          <a href="/">All Data</a>
         </li>
         <li>
-          <a href="#">Averages</a>
+          <a href="/">Averages</a>
         </li>
       </ul>
     </div>
