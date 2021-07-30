0ba52c6399ccbc26845b1d04e4cec8c2edeb7cd3
Neskoff <neskoff@gmail.com>
2021-07-30 16:20:40 +0200

    Show data on Averages page #27

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-
-const rootReducer = combineReducers();
+import averageValues from "./averageValuesReducer";
+const rootReducer = combineReducers({ averageValues });
 
 export default rootReducer;
